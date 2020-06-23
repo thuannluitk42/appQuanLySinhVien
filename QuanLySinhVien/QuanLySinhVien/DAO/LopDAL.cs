863bbf31ef948f194fbf24f126220940e83b7799
Lê Văn Thuận <44367274+ThuanK42@users.noreply.github.com>
2020-06-23 22:36:58 +0700

    update

---
+++ QuanLySinhVien/QuanLySinhVien/DAO/LopDAL.cs
@@ -0,0 +1,100 @@
+﻿using QuanLySinhVien.data;
+using QuanLySinhVien.Object;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySinhVien.Service
+{
+    class LopDAL
+    {
+        DataConnection dc;
+        SqlDataAdapter da;
+        SqlCommand cmd;
+        public LopDAL()
+        {
+            dc = new DataConnection();
+        }
+
+        public DataTable getAllClass()
+        {
+            // tao cau lenh SQL
+            string sql = "Select * from Lop";
+            // khoi tao ket noi
+            SqlConnection con = dc.getConnection();
+            // Khoi tao doi tuong lop SqlDataAdapter
+            con.Open();
+            // do du lieu tu SqlDataAdapter -> datatable
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            // dong ket noi
+            con.Close();
+            return dt;
+
+        }
+
+        public bool insertClass(Lop lop)
+        {
+            string sql = "INSERT INTO dbo.Lop VALUES(@MaLop, @TenLop)";
+            SqlConnection con = dc.getConnection();
+            try
+            {
+                cmd = new SqlCommand(sql, con);
+                con.Open();
+                cmd.Parameters.Add("@MaLop", SqlDbType.NVarChar).Value = lop.MaLop;
+                cmd.Parameters.Add("@TenLop", SqlDbType.NVarChar).Value = lop.TenLop;
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool updateClass(Lop lop)
+        {
+            string sql = "Update dbo.Lop set TenLop = @TenLop where MaLop = @MaLop";
+            SqlConnection con = dc.getConnection();
+            try
+            {
+                cmd = new SqlCommand(sql, con);
+                con.Open();
+                cmd.Parameters.Add("@MaLop", SqlDbType.NVarChar).Value = lop.MaLop;
+                cmd.Parameters.Add("@TenLop", SqlDbType.NVarChar).Value = lop.TenLop;
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool deleteClass(Lop lop)
+        {
+            string sql = "delete Lop where MaLop = @MaLop";
+            SqlConnection con = dc.getConnection();
+            try
+            {
+                cmd = new SqlCommand(sql, con);
+                con.Open();
+                cmd.Parameters.Add("@MaLop", SqlDbType.NVarChar).Value = lop.MaLop;
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
---
+++ QuanLySinhVien/QuanLySinhVien/Service/LopBLL.cs
@@ -0,0 +1,37 @@
+﻿using QuanLySinhVien.Object;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySinhVien.Service
+{
+    class LopBLL
+    {
+        LopDAL lopDAL;
+        public LopBLL()
+        {
+            lopDAL = new LopDAL();
+        }
+
+        public DataTable getAllClass()
+        {
+           return lopDAL.getAllClass();
+        }
+        public bool insertClass(Lop lop)
+        {
+            return lopDAL.insertClass(lop);
+        }
+        public bool updateClass(Lop lop)
+        {
+            return lopDAL.updateClass(lop);
+        }
+
+        public bool deleteClass(Lop lop)
+        {
+            return lopDAL.deleteClass(lop);
+        }
+    }
+}
